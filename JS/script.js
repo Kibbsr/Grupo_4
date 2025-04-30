ec1cec77b23cb8280d6cdc3b0def61709bccf174
Kibssr <kikes4606@gmail.com>
2025-04-30 09:18:58 -0300

    troca de botões "onclick" no html por adição de eventos dentro do javascript

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,20 @@
-function abrir() {
-    const menu = document.getElementById("menuLateral");
-    menu.classList.add("aberto"); 
-}
+document.addEventListener("DOMContentLoaded", function () {
+    // Aqui entram as funções que devem ser executadas no instante em que a página é carregada
+    const botaoAbrir = document.querySelector(".abrir-menu");
+    const botaoFechar = document.querySelector(".fechar-menu");
+    const menuLateral = document.getElementById("menuLateral");
+    const botaoVoltar = document.getElementById("botaovoltar");
 
-function fechar() {
-    const menu = document.getElementById("menuLateral");
-    menu.classList.remove("aberto"); 
-}
+    // Adiciona os manipuladores de evento
+    botaoAbrir.addEventListener("click", function () {
+        menuLateral.classList.add("aberto");
+    });
 
-function voltar() {
-    window.history.back();
-}
+    botaoFechar.addEventListener("click", function () {
+        menuLateral.classList.remove("aberto");
+    });
+
+    botaoVoltar.addEventListener("click", function () {
+        window.history.back();
+    })
+});
