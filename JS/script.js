f825a520b66bcba37f47bb10159dd705474cac14
Kibssr <kikes4606@gmail.com>
2025-04-30 09:20:28 -0300

    up

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Aqui entram as funções que devem ser executadas no instante em que a página é carregada
+
+    // Declaração dos botões
     const botaoAbrir = document.querySelector(".abrir-menu");
     const botaoFechar = document.querySelector(".fechar-menu");
     const menuLateral = document.getElementById("menuLateral");
     const botaoVoltar = document.getElementById("botaovoltar");
 
-    // Adiciona os manipuladores de evento
+    // Manipuladores de evento
     botaoAbrir.addEventListener("click", function () {
         menuLateral.classList.add("aberto");
     });
-
     botaoFechar.addEventListener("click", function () {
         menuLateral.classList.remove("aberto");
     });
-
     botaoVoltar.addEventListener("click", function () {
         window.history.back();
     })
